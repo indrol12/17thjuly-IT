68e9d1b2f6e994617b5477a49eecbb4835aa38aa
indrol12 <indrolofficial@gmail.com>
2024-07-31 11:57:01 +0530

    simple java file committed

---
+++ simple.java
@@ -0,0 +1,15 @@
+public class simple{
+
+public static void main(String args[])
+{
+
+int a=50;
+int b=80;
+
+System.out.println("first number  :"+a);
+System.out.println("second number :"+b);
+
+System.out.println("Addition is :"+(a+b));
+
+}
+}
